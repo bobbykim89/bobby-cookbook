ef127a9dc55c04f205755ac4848d895ee4d45965
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-01-13 18:11:25 -0700

    added dummy layout of profile page

---
+++ components/profile-parts/ProfileNav.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="w-full">
+    <div
+      class="w-[95%] lg:w-full max-w-md p-2 mx-auto bg-white border border-gray-200 rounded-2xl shadow-lg"
+    >
+      <button
+        @click="toggleNav"
+        class="flex justify-between w-full px-4 py-2 text-2xl font-semibold text-left text-white bg-[#f1ac18] rounded-lg hover:bg-[#f25b0a] items-center"
+      >
+        <span>Categories</span>
+        <i
+          v-bind:class="[
+            navOpen ? 'transform rotate-180' : '',
+            'fas fa-chevron-up w-5 h-5 text-white',
+          ]"
+        />
+      </button>
+      <div
+        v-if="navOpen"
+        class="px-4 pt-4 pb-2 text-xl text-gray-600 font-semibold"
+      >
+        <button
+          class="block mb-2 text-center hover:text-gray-900 transition ease-in duration-150"
+        >
+          MyPosts
+        </button>
+        <button
+          class="block mb-2 text-center hover:text-gray-900 transition ease-in duration-150"
+        >
+          Liked Posts
+        </button>
+        <button
+          class="block mb-2 text-center hover:text-gray-900 transition ease-in duration-150"
+        >
+          Update profile
+        </button>
+        <button
+          class="block mb-2 text-center hover:text-gray-900 transition ease-in duration-150"
+        >
+          Change Password
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navOpen: true,
+    }
+  },
+  methods: {
+    toggleNav() {
+      this.navOpen = !this.navOpen
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/profile/_uid/index.vue
+++ pages/profile/_uid/index.vue
@@ -1,7 +1,69 @@
 <template>
-  <h1>A profile page! {{ this.$route.params.uid }}</h1>
+  <section>
+    <div
+      class="w-full items-center shadow-lg mb-20 relative grid w-full h-[40vh] lg:h-[30vh] h-full"
+    >
+      <img
+        :src="this.imageUrl"
+        alt="vegetables"
+        class="absolute object-cover h-full w-full object-center overflow-hidden opacity-80"
+      />
+      <div class="relative inline-block mx-auto self-center text-center">
+        <img
+          :src="this.defaultProfile"
+          alt="profile"
+          class="w-28 h-28 object-cover rounded-full border-2 border-white inline-block mb-4"
+        />
+        <h1
+          class="self-center text-white font-semibold text-xl capitalize text-shadow-lg"
+        >
+          {{
+            this.$store.state.authStore.user &&
+            this.$store.state.authStore.user.displayName
+          }}
+        </h1>
+      </div>
+    </div>
+    <div class="lg:w-[90%] mx-auto">
+      <div class="grid grid-flow-row lg:grid-cols-4 gap-8 mb-20">
+        <div class="w-full">
+          <ProfileNav />
+        </div>
+        <div class="lg:col-span-3 grid gap-4">
+          <h1 class="text-4xl font-bold mb-8 ml-3">My Recipes</h1>
+          <div class="grid lg:grid-cols-3 gap-4">
+            <Card
+              v-for="recipe in loadedPosts"
+              :key="recipe.id"
+              :post="recipe"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import ProfileNav from '@/components/profile-parts/ProfileNav.vue'
+import Card from '@/components/recipe-parts/Card.vue'
+export default {
+  components: {
+    ProfileNav,
+    Card,
+  },
+  data() {
+    return {
+      imageUrl:
+        'https://images.unsplash.com/photo-1542223189-67a03fa0f0bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1997&q=80',
+      defaultProfile: '/images/defaultProfile.jpg',
+    }
+  },
+  computed: {
+    loadedPosts() {
+      //   return this.$store.getters['postsStore/getPosts']
+      return this.$store.state.postsStore.recipes
+    },
+  },
+}
 </script>
